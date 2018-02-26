d98bd2c205b8f599bb9268228dd8ab0eeb7ee3b5
Grayson Cowing <grayson.cowing@gmail.com>
2018-02-26 23:03:52 +0000

    automatically backed up by learn

---
+++ lib/song.rb
@@ -0,0 +1,11 @@
+class Song
+  attr_accessor :artist, :name
+  def initialize(name)
+    @name=name
+  end
+
+  def self.new_by_filename(filename)
+    parse = filename.split(" - ")
+    
+  end
+end
